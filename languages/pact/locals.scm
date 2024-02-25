d6850d7591bcfe74d5cebf2da172c665c6ed853e
Salama Ashoush <salamaashoush@gmail.com>
2024-02-26 00:58:28 +0100

    fix config

---
+++ languages/pact/locals.scm
@@ -0,0 +1,38 @@
+; Scopes
+;-------
+
+(defun) @local.scope
+(defcap) @local.scope
+(defpact) @local.scope
+(module) @local.scope
+(let_binding) @local.scope
+(defschema) @local.scope
+(defproperty) @local.scope
+
+(use imports: (list (reference) @local.definition))
+
+(property_identifier) @local.definition
+(parameter_identifier) @local.definition.parameter
+(type_annotation (type_identifier) @local.definition.associated)
+(let_variable_identifier) @local.definition.var
+
+; References
+;------------
+(reference (atom) @local.reference)
+(def_identifier) @local.definition.function
+(schema_property_identifier) @local.definition
+(module_identifier) @local.definition
+(module_governance) @local.reference
+
+(defun (def_identifier) @local.definition.function
+  (set! definition.function.scope "parent")
+)
+(defpact (def_identifier) @local.definition.function
+  (set! definition.function.scope "parent")
+)
+(defcap (def_identifier) @local.definition.function
+  (set! definition.function.scope "parent")
+)
+(module (module_identifier) @local.definition.module
+  (set! definition.module.scope "global")
+)
